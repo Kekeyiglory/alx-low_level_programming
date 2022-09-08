d681a606ee19a5e45af3bc827ad4f86fc616def4
Kekeyiglory <kekeyiglory@gmail.com>
2022-09-08 15:23:58 -0700

    My first commit in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print the program
+ *
+ * Return: Always 0 (It is a success)
+ */
+int main(v0id)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print the file
+ *
+ * Return: Always 0 (As successful)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
