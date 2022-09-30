b7b4165695216fbbcaeb4ef5d287946676270f8c
Kekeyiglory <kekeyiglory@gmail.com>
2022-09-30 06:55:03 -0700

    My static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - function that puts
+ * @str: _puts function parameter
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	 _putchar('\n');
+}
